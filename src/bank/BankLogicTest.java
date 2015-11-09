1e46c5e4cdf4e417dd0cc206602cd1055c9d3d73
Cam <Cam@DELL>
2015-11-09 10:06:46 +0100

    updatera BankLogicTest

--- src/bank/BankLogicTest.java
+++ src/bank/BankLogicTest.java
@@ -27,7 +27,7 @@ public class BankLogicTest {
 		Assert.assertEquals(_banklogic.getNrOfCustomers(), 2);	
 	}	
 
-	// Test om en kund existera och inte existera
+	// Test om en kund existera
 	@Test
 	public void testgetCustomerByPNr() {
 		_banklogic.addCustomer("anna", 12345);
@@ -67,5 +67,129 @@ public class BankLogicTest {
 		_banklogic.addSavingsAccount(pNr);
 		
 		Assert.assertNotNull("Skall inte vara Null", _banklogic.getAccountById( _banklogic.getCustomerByPNr(pNr), 1001 ));
+	}
+
+	// Test ändta namn på en kund
+	@Test
+	public void testchangeCustomerName() {
+		int pNr = 12345;
+
+		// skapa en kund
+		_banklogic.addCustomer("anna", pNr);
+		_banklogic.changeCustomerName("hampus", pNr);
+
+		Assert.assertEquals(_banklogic.getCustomerByPNr(pNr).getName(), "hampus");
+	}
+
+	// Test ta bort en kund
+	@Test
+	public void testremoveCustomer() {
+		int pNr = 12345;
+
+		// skapa en kund
+		_banklogic.addCustomer("anna", pNr);
+
+		// test retur värde som innehåller 2 strämg
+		Assert.assertEquals(_banklogic.removeCustomer(pNr).size(), 2);
+
+		_banklogic.addCustomer("anna", pNr);
+		_banklogic.addSavingsAccount(pNr);
+		_banklogic.addSavingsAccount(pNr);
+
+		// test retur värde som innehåller 4 strämg
+		Assert.assertEquals(_banklogic.removeCustomer(pNr).size(), 4);
+	}
+
+	// Test lägga till konto
+	@Test
+	public void testaddSavingsAccount() {
+		int pNr = 12345;
+
+		// skapa en kund
+		_banklogic.addCustomer("anna", pNr);
+
+		// test om ett konto har skapat
+		Assert.assertEquals(_banklogic.addSavingsAccount(pNr), 1001);
+
+		// test om ett konto inte hade skapat
+		Assert.assertEquals(_banklogic.addSavingsAccount(0000), -1);
+	}
+
+	// hämta ett konto
+	@Test
+	public void testgetAccount() {
+		int pNr = 12345;
+
+		// skapa en kund
+		_banklogic.addCustomer("anna", pNr);
+
+		// skapa ett konto till kunden
+		_banklogic.addSavingsAccount(pNr);
+
+		// hämta ett konto som existera
+		Assert.assertNotNull("Skall inte vara Null", _banklogic.getAccount( pNr, 1001 ));
+
+		// hämta ett konto som inte existera
+		Assert.assertNull("Skall vara Null", _banklogic.getAccount( pNr, 1005 ));
+		Assert.assertNull("Skall vara Null", _banklogic.getAccount( 0000, 1001 ));
+	}
+
+	// Test insättning
+	@Test
+	public void testdeposit() {
+		int pNr = 12345;
+
+		// skapa en kund
+		_banklogic.addCustomer("anna", pNr);
+
+		// skapa ett konto till kunden
+		_banklogic.addSavingsAccount(pNr);
+
+		// Test ett lycka insättning
+		Assert.assertTrue(_banklogic.deposit(pNr, 1001, 10));
+
+		// Test misslycka insättning
+		Assert.assertFalse(_banklogic.deposit(pNr, 1005, 10));
+		Assert.assertFalse(_banklogic.deposit(0000, 1001, 10));
+	}
+
+	// Test uttag
+	@Test
+	public void testwithDraw() {
+		int pNr = 12345;
+
+		// skapa en kund
+		_banklogic.addCustomer("anna", pNr);
+
+		// skapa ett konto till kunden
+		_banklogic.addSavingsAccount(pNr);
+
+		// lägga till 5000 på konto
+		_banklogic.deposit(pNr, 1001, 5000);
+
+		// Test lycka uttag
+		Assert.assertTrue(_banklogic.withdraw(pNr, 1001, 4000));
+
+		// Test misslycka uttag
+		Assert.assertFalse(_banklogic.withdraw(pNr, 1001, 1000000));
+	}
+
+	// Test stänga konto
+	@Test
+	public void testcloseAccount() {
+		int pNr = 12345;
+
+		// skapa en kund
+		_banklogic.addCustomer("anna", pNr);
+
+		// skapa ett konto till kunden
+		_banklogic.addSavingsAccount(pNr);
+
+		// stänger ett konto som existera
+		Assert.assertNotNull("Skall inte vara Null", _banklogic.closeAccount( pNr, 1001 ));
+
+		// stänger ett konto som inte existera
+		Assert.assertNull("Skall vara Null", _banklogic.closeAccount( pNr, 1005 ));
+		Assert.assertNull("Skall vara Null", _banklogic.closeAccount( 0000, 1001 ));
 	}
 }
