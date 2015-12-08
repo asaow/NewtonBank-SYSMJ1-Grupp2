429cc50dbb760239debeeca2cf88356ba7af0e7e
chq8888 <ca_huy@yahoo.se>
2015-12-08 10:31:50 +0100

    uppdatera SavingsAccount class

--- src/bank/SavingsAccount.java
+++ src/bank/SavingsAccount.java
@@ -3,49 +3,56 @@ package bank;
 public class SavingsAccount extends Account {
 	public static final String ACCOUNT_TYPE = "SPARKONTO";
 
+	private double withdrawRate;
 	/**
 	 * Konstruktor för SavingsAccount, anropar konstruktorn i superklassen
 	 * Account
 	 *
-	 * @param accountId
-	 *            kontonr
-	 * @param rate
-	 *            räntan på kontot
+	 * @param accountId kontonr
+	 * @param rate räntan på kontot
 	 */
 	public SavingsAccount(int accountId, double rate) {
 		super(accountId);
 		super.setType(ACCOUNT_TYPE);
 		super.setRate(rate);
+		withdrawRate = 0;
 	}
-
+	
 	/**
-	 * Uttag från konto
+	 * Sätta uttagsränta om kunden gör flera än ett uttag per år 
+	 * @param rate uttag räntan
+	 */
+	public void setWithdrawRate(double rate) {
+		withdrawRate = rate;
+	}
+	
+	/**
+	 * Uttag ett belopp från konto
 	 * 
-	 * @param amount
-	 *            belopp som skall ta ut på konto
+	 * @param amount belopp som skall ta ut på konto
 	 * @return true om uttag lyckas annars false
-	 * TODO Kontroll av tidpunkt för förra uttaget
 	 */
 	@Override
 	public boolean withdraw(double amount) {
+		if (amount < 0)	return false;
 		
-		if (amount < 0)
-			return false;
-
-		if (getBalance() - amount > 0) {
-			setBalance(getBalance() - amount);
+		double balance = getBalance();
+		double withdraw = amount + (withdrawRate / 100 * amount);
+		
+		if (balance - withdraw >= 0) {
+			setBalance(balance - withdraw);
 			return true;
 		} else
 			return false;
 	}
 
 	/**
-	 * Beräknar och returnerar räntan
-	 * 
 	 * @return ränta på konto
 	 */
 	@Override
 	public double calculateRate() {
 		return getBalance() * getRate() / 100;
 	}
+	
+	
 }
