3b69e9ad8124d11a4a4a7e4a356d18392c04ce5f
BATMUS1 <bohlin.hampus@gmail.com>
2015-12-09 10:46:04 +0100

    Uppdatarat lite namn på filer

---
+++ src/bank/CustomerTest.java
@@ -0,0 +1,34 @@
+package bank;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CustomerTest {
+
+	@Test
+	public void testCustomer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPNr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPNr() {
+		fail("Not yet implemented");
+	}
+
+}
